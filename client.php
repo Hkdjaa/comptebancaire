f2712748b2c32820c51cc095192db052c24363f2
Hadja Aïssatou BALDE <151042235+Hkdjaa@users.noreply.github.com>
2024-05-12 23:16:17 +0000

    Update client.php

--- client.php
+++ client.php
@@ -1,14 +1,18 @@
 <?php
 class client {
+    protected $id_client;
     protected $nom_client;
     protected $Prenom_client;
 
   
-    public function __construct($nom_client, $Prenom_client) {
+    public function __construct($ID_client,$nom_client, $Prenom_client) {
+        $this->ID_client = $ID_client;
         $this->nom_client = $nom_client;
         $this->Prenom_client = $Prenom_client;
     }
-
+    public function getID_client() {
+        return $this->ID_client;
+    }
 
     public function getnom_client() {
         return $this->nom_client;
